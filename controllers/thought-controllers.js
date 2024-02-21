46d5981e675328f8962804f425807020e9daa74f
davidmelendez <Dmelz11@github.com>
2024-02-21 15:56:58 -0800

    added logic to perform Crud operations on user thoughts and reactionsin thought-controllers.js and added .DS_Store to gitignore file

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -67,5 +67,68 @@ module.exports ={
     },
 
     // finds and deletes thoughts from database by id
+async deleteThought(req, res) {
+  try {
+    const thought = await Thought.findOneAndRemove({_id: req.params.thoughtId});
 
+    if (!thought) {
+      return res.status(404).json({message: 'No thought with that id'});
     }
+
+    
+    const user = await User.findOneAndUpdate(
+      { thoughts: req.params.thoughtId },
+      { $pull: {thoughts: req.params.thoughtId} },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({message: 'Thought is created, but there is no user with that id'});
+    }
+    res.json({message: 'Thought was successfully created!'});
+  } catch (err) {
+    console.log(err);
+  }
+},
+// adding a reaction to a user thought
+async addReaction(req,res) {
+  try {
+    const thought = await Thought.findOneAndUpdate(
+      {_id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body }},
+      { runValidators: true, new: true}
+    );
+
+    if(!thought) {
+      return res.status(404).json({message: 'No thought with that id'});
+    }
+
+    res.json(Thought);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+   }
+  },
+  // deleting a reaction from a thought
+async removeReaction(req, res) {
+  try {
+    const thought = await Thought.findOneAndUpdate(
+      {_id: req.params.thoughtId},
+      { $pull: { reactions: {reactionId: req.params.reactionId}}},
+      { runValidators: true, new: true}
+    );
+
+    if(!thought) {
+      return res.status(404).json({message: 'No thought with that id!'});
+    }
+
+    res.json(thought);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+},
+};
+
+module.exports = thoughtController;
+    
